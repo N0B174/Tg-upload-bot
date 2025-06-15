2ea6d830b57fc581e196bac6b6d6a20c5cee78bf
N0B174 <j3rrydcw@gmail.com>
2025-06-15 09:41:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ Bot_name = "🎬 Movie Box 🎬"
 api_id = 21809176
 api_hash = "7f31d1bcfbe3d7f9fd2443c07a1f0607"
 Token = "7645734668:AAHXqcNqIpGcuRSAlaQf3oGZjCEno4eUWMk"
-chanel1_id= -1002082050309
-chanel1_Join_link = "https://t.me/+r_5eHSUOgARhMDQ9"
+chanel1_id= -1002873932698
+chanel1_Join_link = "https://t.me/+1buXgfezqE1iYWU1"
